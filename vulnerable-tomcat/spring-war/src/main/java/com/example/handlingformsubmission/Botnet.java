848d5e558f267b8520658c8c10bc9470e83aaea5
Kevin@Fortinet <kevinfortinet@Kevins-MacBook-Air.local>
2022-10-20 11:39:11 +0800

    add botnet button

---
+++ vulnerable-tomcat/spring-war/src/main/java/com/example/handlingformsubmission/Botnet.java
@@ -0,0 +1,24 @@
+package com.example.handlingformsubmission;
+
+public class Botnet {
+
+	private String ip;
+	private String port;
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public String getPort() {
+		return port;
+	}
+
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+}
--- vulnerable-tomcat/spring-war/src/main/java/com/example/handlingformsubmission/GreetingController.java
+++ vulnerable-tomcat/spring-war/src/main/java/com/example/handlingformsubmission/GreetingController.java
@@ -1,5 +1,9 @@
 package com.example.handlingformsubmission;
 
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +23,24 @@ public class GreetingController {
 	public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
 		model.addAttribute("greeting", greeting);
 		return "result";
+	}
+	@PostMapping("/botnet")
+	public String botnetConnect(Model model) {
+		Botnet botnet = new Botnet();
+		botnet.setIp("1.123.37.68");
+		botnet.setPort("80");
+		model.addAttribute("botnet", botnet);
+		URL url;
+		try {
+			url = new URL("http://1.123.37.68:80");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			System.out.println("Sending request to botnet");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "botnet";
 	}
 
 }
